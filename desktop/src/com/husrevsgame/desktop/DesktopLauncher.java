abadb7ccf402fde6e0468bf0c5c0763efc84f6f7
Hüsrev Yılmaz <31134425+hsrev@users.noreply.github.com>
2018-02-24 17:51:46 +0300

    libgdx dosyalari olusturuldu

---
+++ desktop/src/com/husrevsgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.husrevsgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.husrevsgame.RaceCar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RaceCar(), config);
+	}
+}
